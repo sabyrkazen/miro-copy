8a814bbb72702e30661c884286b1126e87a3e24a
Sabyrka <sabyrkazen@gmail.com>
2026-02-15 01:03:35 +0500

    chore: add Prettier for code formatting and update auth component to remove semicolons

---
+++ prettier.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from 'prettier'
+
+const config: Config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+}
+
+export default config
--- src/features/auth/index.tsx
+++ src/features/auth/index.tsx
@@ -1,7 +1,7 @@
 export function LoginPage() {
-  return <div>Login page</div>;
+  return <div>Login page</div>
 }
 
 export function RegisterPage() {
-  return <div>Register page</div>;
+  return <div>Register page</div>
 }
